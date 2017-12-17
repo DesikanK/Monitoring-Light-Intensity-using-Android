131540dc3d63be11310f7bc3638a02cfe4d4f8e0
DesikanK <34052112+DesikanK@users.noreply.github.com>
2017-12-18 00:26:50 +0530

    Add files via upload

---
+++ BH1750.ino
@@ -0,0 +1,33 @@
+#include <BH1750.h>
+#include <Wire.h>
+
+BH1750 lightSensor;
+unsigned long sampleTimer = 0;
+unsigned long sampleInterval = 100; //100 ms = 10Hz rate
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Wire.begin();
+
+  lightSensor.begin(BH1750_CONTINUOUS_HIGH_RES_MODE);// Measurement time is approx 120ms.
+  Serial.println(F("Light Intensity"));
+
+}
+
+void loop() 
+{
+  uint16_t lux = lightSensor.readLightLevel();
+  unsigned long currMillis = millis();
+  if(currMillis - sampleTimer >= sampleInterval)
+  {
+     sampleTimer = currMillis; 
+     Serial.print("Light: ");
+     Serial.print(lux);
+     Serial.println(" lx"); 
+  }
+  
+  delay(1000); //delay of 1 sec
+
+}
